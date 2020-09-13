cefb36e19aaa3a38f365eedbda4a679ba54b9ba5
Hiroki Sakuraba <feath.7615@gmail.com>
2020-09-13 16:10:44 +0900

    OSを判別できるようにした

---
+++ pkg/internal/backend/detect.go
@@ -0,0 +1,42 @@
+package backend
+
+import (
+	"strings"
+
+	"github.com/raba-jp/primus/pkg/internal/exec"
+	"github.com/spf13/afero"
+)
+
+type OS int
+
+const (
+	Unknown OS = iota + 1
+	Darwin
+	Manjaro
+)
+
+func DetectOS(execIF exec.Interface, fs afero.Fs) OS {
+	if darwin := DetectDarwin(execIF); darwin {
+		return Darwin
+	}
+	if manjaro := DetectArchLinux(fs); manjaro {
+		return Manjaro
+	}
+	return Unknown
+}
+
+func DetectDarwin(execIF exec.Interface) bool {
+	out, err := execIF.Command("uname", "-a").Output()
+	if err != nil {
+		return false
+	}
+	if !strings.Contains(string(out), "Darwin") {
+		return false
+	}
+	return true
+}
+
+func DetectArchLinux(fs afero.Fs) bool {
+	_, err := fs.Stat("/etc/arch-release")
+	return err == nil
+}
