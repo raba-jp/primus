387d1ac2552026614c88bcc587f52bea135c5ee6
Hiroki Sakuraba <feath.7615@gmail.com>
2020-09-20 18:16:22 +0900

    Add completions

--- pkg/internal/promptlib/repl.go
+++ pkg/internal/promptlib/repl.go
@@ -33,10 +33,16 @@ func NewPrompt() {
 
 func NewCompeter(d prompt.Document) []prompt.Suggest {
 	s := []prompt.Suggest{
-		{Text: "command", Description: "Execute command"},
-		{Text: "file_copy", Description: "File copy"},
-		{Text: "file_move", Description: "File move"},
+		{Text: "execute", Description: "Execute command. `execute('cmd', ['option1', 'option2'])`"},
+		{Text: "file_copy", Description: "File copy. `file_copy('/a/src.txt', '/b/dest.txt')`"},
+		{Text: "file_move", Description: "File move. `file_move('/a/src/txt', '/b/dest.txt')`"},
+		{Text: "fish_set_path", Description: "Set $PATH. `fish_set_path(['$GOPATH/bin', '$HOME/.bin'])`"},
+		{Text: "fish_set_varialbe", Description: "Set variable. `fish_set_variable('GOPATH', '$HOME/go')`"},
+		{Text: "http_request", Description: "Send HTTP request. `http_request('https://example.com', '$HOME/example.html')`"},
+		{Text: "package", Description: "Install package. `package('base-devel')`"},
+		{Text: "symlink", Description: "Create symbolic link. `symlink('/a/src.txt', '/b/dest.txt')`"},
 	}
+
 	return prompt.FilterFuzzy(s, d.GetWordBeforeCursor(), true)
 }
 
